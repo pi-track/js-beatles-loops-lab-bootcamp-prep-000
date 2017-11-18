81659a08a2ce91fe4d252204672e39ab9485fde1
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-18 22:23:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i<musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  for (let i = 0; i<facts.length; i++){
+    facts.splice(i,0,facts[i] + "!!!")
+  }
+  return facts
+}
